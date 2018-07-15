97603b85aef5247e56beea110eeaae484ee4ec84
RolaChen <976950078@qq.com>
2018-07-15 16:18:56 +0800

    ok

--- bignumber.h
+++ bignumber.h
@@ -4,7 +4,7 @@
 #include<cstring>
 #include<string>
 #include<algorithm>
-<<<<<<< HEAD
+#include<stdlib.h>
 using namespace std;
 
 string dezero(string);//erase the zeros in the front of a positive number
@@ -25,28 +25,6 @@ private:
 	string a;
 	string b;
 	char sign;
-=======
-#include<stdlib.h>
-using namespace std;
-
-string dezero(string);//erase the zeros in the front of a positive number
-int judge(string,string); 
-string Minus(string,string);
-string Divide(string,string);
-string Multiply(string,string);
- string Add(string,string);
-class bignumber
-{
-public:
- bignumber();
- ~bignumber();
- void readline();
- void print();
-private:
- string a;
- string b;
- char sign;
->>>>>>> f9159e07d644d9f53140b629451b2af7b7220c0c
 };
 
 #endif
--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 
 int main()
 {
+	cout << "Please enter the formula like '34234+4354534='" << endl;
+	cout << "If you want to exist, please enter 'END'" << endl << endl;
 	bignumber test;
 	while (1)
 		test.readline();
